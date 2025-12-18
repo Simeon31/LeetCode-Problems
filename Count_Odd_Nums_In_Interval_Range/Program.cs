e9dcb1f3d306fe5b314b7949eb654b7fb64429b4
Simeon M <simionsimion568@gmail.com>
2025-12-18 13:53:44 +0100

    new problems added

---
+++ Count_Odd_Nums_In_Interval_Range/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Count_Odd_Nums_In_Interval_Range
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution testSolution = new Solution();
+
+            Console.Write("Low = ");
+            int low = int.Parse(Console.ReadLine());
+
+            Console.Write("High = ");
+            int high= int.Parse(Console.ReadLine());
+
+            int output01 = testSolution.CountOddsMethod02(low, high);
+            int ouput02 = testSolution.CountOddsMethod01(low, high);
+
+            Console.WriteLine("First Output: {0}", output01);
+            Console.WriteLine("Second Output: {0}", ouput02);
+        }
+    }
+
+    public class Solution
+    {
+        public int CountOddsMethod01(int low, int high)
+        {
+            return (high + (high & 1) - low + (low & 1)) >> 1;
+        }
+
+        public int CountOddsMethod02(int low, int high)
+        {
+            int output = 0;
+            if (0 <= low && high <= int.MaxValue)
+            {
+                while (low <= high)
+                {
+
+                    if (low % 2 == 1)
+                    {
+                        output++;
+                    }
+
+                    low++;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Number out of boundaries");
+                return -1;
+            }
+
+            return output;
+        }
+    }
+}
---
+++ Longest_Common_Prefix/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Longest_Common_Prefix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution sol = new Solution();
+
+            string[] arr = new string[] { "flower", "flow", "flight" };
+
+            Console.WriteLine(sol.LongestCommonPrefix(arr));
+        }
+    }
+
+    public class Solution
+    {
+        public string LongestCommonPrefix(string[] strs)
+        {
+            Array.Sort(strs);
+
+            string firstWord = strs[0];
+            string lastWord = strs[strs.Length - 1];
+
+            int minLength = Math.Min(firstWord.Length, lastWord.Length);
+
+            int i = 0;
+            while (i < minLength && firstWord[i] == lastWord[i])
+            {
+                i++;
+            }
+
+            return lastWord.Substring(0, i);
+        }
+    }
+}
